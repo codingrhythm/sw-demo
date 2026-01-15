0fd4c7c089c3bf0b0fee055b7ae40a3c4fc9a879
Yuzhou Zhu <yuzhou@canva.com>
2026-01-15 14:14:19 +1100

    Add basic test pages

---
+++ sw.js
@@ -0,0 +1,13 @@
+// Minimal service worker for testing
+self.addEventListener('install', function(e) {
+    console.log('SW installing');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('SW activated');
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('SW fetch');
+});
