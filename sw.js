a8a93276003aee77168052bf754c0eb1ec13e548
Yuzhou Zhu <yuzhou@canva.com>
2026-01-19 09:26:36 +1100

    Update

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 // Minimal service worker for testing
 self.addEventListener('install', function(e) {
-    console.log('SW installing');
+    console.log('[SW] installing');
     self.skipWaiting();
 });
 
 self.addEventListener('activate', function(e) {
-    console.log('SW activated');
+    console.log('[SW] activated');
 });
 
 self.addEventListener('fetch', function(e) {
-    console.log('SW fetch');
+    console.log('[SW] fetch');
 });
